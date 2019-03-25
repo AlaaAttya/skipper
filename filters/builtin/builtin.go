5a17fae06af361be8ba03b5403ff146d5925af32
Alexey Ermakov <alexey.ermakov@zalando.de>
2019-03-25 17:25:07 +0100

    Add cookieToHeader filter
    Signed-off-by: Alexey Ermakov <alexey.ermakov@zalando.de>

--- filters/builtin/builtin.go
+++ filters/builtin/builtin.go
@@ -59,6 +59,7 @@ const (
 	InlineContentName   = "inlineContent"
 	HeaderToQueryName   = "headerToQuery"
 	QueryToHeaderName   = "queryToHeader"
+	CookieToHeaderName  = "cookieToHeader"
 )
 
 // Returns a Registry object initialized with the default set of filter
@@ -94,6 +95,7 @@ func MakeRegistry() filters.Registry {
 		NewCopyResponseHeader(),
 		NewHeaderToQuery(),
 		NewQueryToHeader(),
+		NewCookieToHeader(),
 		NewSetDynamicBackendHostFromHeader(),
 		NewSetDynamicBackendSchemeFromHeader(),
 		NewSetDynamicBackendUrlFromHeader(),
